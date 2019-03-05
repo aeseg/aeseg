679bff9eccc6a791e3a5ff266538b78e0b342334
leocances <leocances@gmail.com>
2019-03-05 10:56:45 +0100

    Add the main encode function, write better documentation and now complies with PEP8 standard

--- Encoder.py
+++ Encoder.py
@@ -2,18 +2,18 @@ import numpy as np
 
 
 class Encoder:
-    """
-    In a sound event detection task, the output of the prediction model is
+    """In a sound event detection task, the output of the prediction model is
     often a temporal prediction. Different segmentation algorithm exist in order
     to translate this curves into a list of segment.
     """
+
     def __init__(self, classes: list, temporal_precision: int, clip_length: int,
                  minimal_segment_step: int):
         """
         Args:
             classes (list):
             temporal_precision (int):
-            clip_length (int):
+            clip_length (int): The length of the audio file (in seconds)
             minimal_segment_step (int):
         """
         self.classes = classes
@@ -21,12 +21,93 @@ class Encoder:
         self.clip_length = clip_length
         self.minimal_segment_step = minimal_segment_step
 
+        # Attribute that are not initialize with the constructor
+        self.frame_length = None
+        self.nb_frame = None
+
+    def encode(self, temporal_prediction: np.array, method: str = "threshold",
+               smooth: str = None, **kwargs) -> str:
+        """Perform the localization of the sound event present in the file.
+
+        Using the temporal prediction provided y the last step of the system,
+        it will "localize" the sound event inside the file under the form of a
+        strongly annotated line. (see DCASE2018 task 4 strong label exemple).
+        There is two methods implemented here, one using a simple threshold
+        based segmentation and an other using a modulation system based on the
+        variance of the prediction over the time.
+
+        # Exemples
+        ```YOTsn73eqbfc_10.000_20.000.wav  0.163   0.665   Alarm_bell_ringing```
+
+        Args:
+            temporal_prediction (np.array): A 3-dimension numpy array (<nb
+                clip>, <nb frame>, <nb class>)
+            method (str): The segmentation method to use [threshold | hysteresis
+                | derivative | primitive]
+            smooth (str): The smoothing method to use [smoothMovingAvg]
+            kwargs: See the segmentation method parameters
+
+        Returns:
+            Return a list of positive and negative segments with their size. A
+            segment is a tuple where the first value that represent the segment
+            value (1) for positive, (0) for negative and the second values is
+            the width of the segment (number of frame)
+        """
+        # parameters verification
+        _methods = ["threshold", "hysteresis", "derivative", "primitive",
+                    "mean_threshold", "median_threshold", "dynamic_threshold",
+                    "global_mean_threshold", "global_median_threshold"]
+
+        if method not in _methods:
+            raise ValueError("Method %s doesn't exist. Only %s are available" %
+                             (method, _methods))
+
+        # Depending on the method selected, the proper function will be selected
+        if method == _methods[0]:
+            encoder = self.__encode_using_threshold
+        elif method == _methods[2]:
+            encoder = self.__encodeUsingDerivative
+        elif method == _methods[1]:
+            encoder = self.__encodeUsingHysteresis
+        elif method == _methods[3]:
+            encoder = self.__encodeUsingPrimitive
+        elif method == _methods[4]:
+            encoder = self.__encodeUsingMeanThreshold
+        elif method == _methods[7]:
+            encoder = self.__encode_using_global_mean_threshold
+        elif method == _methods[5]:
+            encoder = self.__encodeUsingMedianTreshold
+        elif method == _methods[6]:
+            encoder = self.__encodeUsingDynamicThreshold
+        elif method == _methods[8]:
+            encoder = self.__encode_using_global_median_threshold
+        else:
+            encoder = None
+
+        # Apply smoothing if requested
+        if smooth is not None:
+            temporal_prediction = self.__smooth(temporal_prediction,
+                                                method=smooth, **kwargs)
+
+        # Now that we have the strong prediction, we can assign the value to the
+        # two attributes nb_frame and frame_length
+        self.nb_frame = temporal_prediction.shape[1]
+        self.frame_length = self.clip_length / self.nb_frame
+
+        # Execute the selected segmentation algorithm and recover its results
+        return encoder(temporal_prediction, **kwargs)
+
     def __encode_using_threshold(self, temporal_prediction: np.array,
                                  **kwargs) -> list:
         """
+        A basic threshold algorithm. Each value that are above the threshold
+        will be part of a valid segment, an invalid one otherwise.
+
         Args:
             temporal_prediction (np.array):
-            **kwargs:
+            thresholds (list): The list of threshold to apply. It must
+            contain as much threshold that there is classes. (one threshold for
+            each class.
         """
         output = []
 
@@ -56,7 +137,7 @@ class Encoder:
 
                 # first pass --> Fill the holes
                 for i in range(len(bin_prediction_per_class) - max_hole_size):
-                    window = bin_prediction_per_class[i: i+max_hole_size]
+                    window = bin_prediction_per_class[i: i + max_hole_size]
 
                     if window[0] == window[-1] == 1:
                         window[:] = [window[0]] * max_hole_size
